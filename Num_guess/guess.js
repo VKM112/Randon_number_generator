28b458a5a832480c5601ef5c3516819edb315e5d
Mohan <vkmohan112@gmail.com>
2025-05-25 21:51:55 +0530

    4rth commit fix some errors and added css

--- Num_guess/guess.js
+++ Num_guess/guess.js
@@ -1,20 +1,35 @@
-const max_num=document.getElementById("max")
-const min_num=document.getElementById("min")
+const max_num =document.getElementById("max")
+const min_num =document.getElementById("min")
 const button =document.getElementById("btn")
-const guessed_num=document.getElementById("guess")
-const result=document.getElementById("output")
+const guessed_num =document.getElementById("guess")
+const result =document.getElementById("output")
 
 button.onclick=function(){
-    const start=parseInt(max_num.value);
-    const end=parseInt(min_num.value);
+    const start=parseInt(min_num.value);
+    const end=parseInt(max_num.value);
     const guess=parseInt(guessed_num.value)
 
     const rand_guess=Math.floor(Math.random()*(end-start+1)+start);
 
-    if (guess == rand_guess){
-        result.textContent=`YAY You guessed the correct Num`;
+    if (isNaN(start) || isNaN(end) || isNaN(guess)){
+        result.textContent="please enter a valid number!";
+        return;
+    }
+
+    if (start >= end){
+        result.textContent="the minimum range must be less than the maximum number!";
+        return;
+    }
+
+    if (guess < start || guess > end){
+        result.textContent=`the guessed number should be in between ${min} & ${max}`;
+        return;
+    }
+
+    if (guess === rand_guess){
+        result.textContent=`Yay! You Guessed the correct number`;
     }
     else{
-        result.textContent=`Not the correct. number was ${rand_guess}. Please try again!`;
+        result.textContent=`You guessed the wrong number the number is ${rand_guess}`;
     }
 }   
